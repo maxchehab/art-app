2ab1f63efbd0ad5d036e47e2fd6b050c1c513622
Max Chehab <maxchehab@gmail.com>
2017-09-22 14:48:50 -0700

    Implement card class.
    This forces the program to become more component based following the
    Facebook coding guidlines.

---
+++ components/card/Card.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+import {
+     AppRegistry,
+     StyleSheet,
+     Text,
+     Image,
+     View,
+     TouchableOpacity,
+     ScrollView,
+     Dimensions
+} from 'react-native';
+
+import FlipCard from 'react-native-flip-card';
+
+export default class Card extends Component {
+     constructor(props) {
+          super(props)
+     }
+
+     render() {
+          return (
+               <View>
+                    <FlipCard flip={false} friction={6} perspective={1000} flipHorizontal={true} flipVertical={false} clickable={true} style={styles.card} onFlipEnd={(isFlipEnd) => {
+                         console.log('isFlipEnd', isFlipEnd)
+                    }}>
+                         {/* Face Side */}
+                         <View style={styles.face}>
+                              <Text>{this.props.name}</Text>
+                         </View>
+                         {/* Back Side */}
+
+                         <View style={styles.back}>
+                              <Image style={styles.image} source={{
+                                   uri: this.props.uri
+                              }}/>
+                         </View>
+                    </FlipCard>
+               </View>
+          );
+     }
+}
+
+const styles = StyleSheet.create({
+     card: {
+          margin: 0,
+          height: (Dimensions.get('window').width) / 4,
+          width: (Dimensions.get('window').width) / 4
+     },
+     face: {
+          flex: 1,
+          backgroundColor: 'powderblue',
+          justifyContent: 'center',
+          alignItems: 'center'
+
+     },
+     back: {
+          flex: 1,
+          backgroundColor: 'transparent',
+          justifyContent: 'center',
+          alignItems: 'center'
+     },
+
+     image: {
+          width: (Dimensions.get('window').width) / 4,
+          height: (Dimensions.get('window').width) / 4
+     }
+});
+
+AppRegistry.registerComponent('Card', () => Card);
--- index.android.js
+++ index.android.js
@@ -10,8 +10,7 @@ import {
      Dimensions
 } from 'react-native';
 
-import FlipCard from 'react-native-flip-card';
-import {Col, Row, Grid} from "react-native-easy-grid";
+import Card from './components/card/Card.js'
 
 export default class art extends Component {
 
@@ -20,7 +19,6 @@ export default class art extends Component {
 
           this.state = {
                loading: true,
-               width: Dimensions.get('window').width,
                images: []
           }
      }
@@ -31,25 +29,7 @@ export default class art extends Component {
                return response.json();
           }).then(function(json) {
                let Images = json.images.map((image, key) => {
-                    return (
-                         <View key={key} style={styles.cContainer}>
-                              <FlipCard flip={false} friction={6} perspective={1000} flipHorizontal={true} flipVertical={false} clickable={true} style={styles.card} onFlipEnd={(isFlipEnd) => {
-                                   console.log('isFlipEnd', isFlipEnd)
-                              }}>
-                                   {/* Face Side */}
-                                   <View style={styles.face}>
-                                        <Text>{image.name}</Text>
-                                   </View>
-                                   {/* Back Side */}
-
-                                   <View style={styles.back}>
-                                        <Image style={styles.image} source={{
-                                             uri: image.uri
-                                        }}/>
-                                   </View>
-                              </FlipCard>
-                         </View>
-                    );
+                    return (<Card key={key} name={image.name} uri={image.uri}/>);
                });
                that.setState({images: Images, loading: false});
           }).catch(function(err) {
@@ -77,29 +57,6 @@ const styles = StyleSheet.create({
           flex: 1,
           flexDirection: 'row',
           flexWrap: 'wrap'
-     },
-     card: {
-          margin: 0,
-          height: (Dimensions.get('window').width) / 4,
-          width: (Dimensions.get('window').width) / 4
-     },
-     face: {
-          flex: 1,
-          backgroundColor: 'powderblue',
-          justifyContent: 'center',
-          alignItems: 'center'
-
-     },
-     back: {
-          flex: 1,
-          backgroundColor: 'steelblue',
-          justifyContent: 'center',
-          alignItems: 'center'
-     },
-
-     image: {
-          width: (Dimensions.get('window').width) / 4,
-          height: (Dimensions.get('window').width) / 4
      }
 });
 
