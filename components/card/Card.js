927927ba314d6b40516c5938a1f59f985f6f0ecb
Max Chehab <maxchehab@gmail.com>
2017-09-25 15:14:50 -0700

    Implemented modal and autoupdate feature.
    Modal will popup when a match has been made.
    Phone will update data when a new version is created.

--- components/card/Card.js
+++ components/card/Card.js
@@ -17,29 +17,20 @@ export default class Card extends Component {
      }
 
      render() {
-          let images = [
-               require("../../assets/dynamic/afremov.jpg"),
-               require("../../assets/dynamic/gothic.jpg"),
-               require("../../assets/dynamic/mona-lisa.jpg"),
-               require("../../assets/dynamic/saturn.png"),
-               require("../../assets/dynamic/scream.jpg"),
-               require("../../assets/dynamic/self-portrait.jpg"),
-               require("../../assets/dynamic/seurat.jpg"),
-               require("../../assets/dynamic/van-gough.gif")
-          ];
+
 
           return (
                <View>
                     <FlipCard source={this.props.source} ref={(card) => {
                          this._card = card;
                     }} flip={false} friction={6} compare={this.props.compare} perspective={1000} flipHorizontal={true} flipVertical={false} clickable={true} style={styles.card} onFlipEnd={(isFlipEnd) => {
-                         this.props.onFlipEnd(this.props.source, isFlipEnd, this._card);
+                         this.props.onFlipEnd(this.props.index, isFlipEnd, this._card);
                     }}>
                          <View style={styles.front}>
                               <Image style={styles.frontImage} source={require('../../assets/static/question.png')}/>
                          </View>
                          <View style={styles.back}>
-                              <Image style={styles.backImage} source={images[this.props.source]}/>
+                              <Image style={styles.backImage} source={{uri:this.props.source}}/>
                          </View>
                     </FlipCard>
                </View>
--- components/gameboard/GameBoard.js
+++ components/gameboard/GameBoard.js
@@ -1,19 +1,54 @@
 import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, ScrollView, View} from 'react-native';
+import {
+     AppRegistry,
+     StyleSheet,
+     ScrollView,
+     Modal,
+     View,
+     AsyncStorage,
+     Image,
+     TouchableOpacity,
+     TouchableWithoutFeedback,
+     TouchableHighlight,
+     Text
+} from 'react-native';
 
 import Card from '../card/Card.js';
 
 export default class GameBard extends Component {
+
+     //TODO make images async.
      constructor(props) {
           super(props);
+
           this.state = {
-               images: this.generateImages()
+               images: [],
+               imagesData:[],
+               loading: true,
+               modalVisible: false,
+               modalTitle: "[title]",
+               modalDescription: "[description]",
+               modalSource: "https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2016/03/10/18/mona-lisa.jpg"
           }
+          this.generateImages();
      }
 
      Game = {
           selection: -1,
           selectCard: null
+     }
+
+     setModalVisible(visible, id) {
+          if (id != null) {
+               //console.warn(id)
+               for (var i = 0; i < this.state.imagesData.length; i++) {
+                    if (this.state.imagesData[i].id == id) {
+                         this.setState({modalTitle: this.state.imagesData[i].name, modalDescription: this.state.imagesData[i].description, modalSource: this.state.imagesData[i].source})
+                         break;
+                    }
+               }
+          }
+          this.setState({modalVisible: visible});
      }
 
      CompletedCards = [];
@@ -28,13 +63,13 @@ export default class GameBard extends Component {
                          // alert("Match!");
                          this.Game.selectCard.setState({clickable: false});
                          card.setState({clickable: false});
+                         this.setModalVisible(true, key);
                          this.CompletedCards.push(this.Game.selectCard, card);
                     } else {
                          // alert("No Match!");
                          this.Game.selectCard._toggleCard();
                          card._toggleCard();
                     }
-
                     this.Game.selection = -1;
                     this.Game.selectCard = null;
                }
@@ -50,48 +85,105 @@ export default class GameBard extends Component {
                card._toggleCard();
           }
 
-          if(!card.selected){
+          if (this.Game.selectCard == card && card.selected != true) {
+               this.Game.selectCard = null;
+               this.Game.selection = -1;
+          }
+
+          if (!card.selected) {
                card._toggleCard();
                return;
           }
      }
 
      generateImages() {
-          //var images = require('../../assets/dynamic/images.json');
-          var images = [
-               0,
-               1,
-               2,
-               3,
-               4,
-               5,
-               6,
-               7
-          ];
-          //Randomize the entire array
-          images.sort(function() {
-               return 0.5 - Math.random()
-          });
+          const update = async() => {
+               let version = 0;
+               try {
+                    const value = await AsyncStorage.getItem('version');
+                    if (value !== null) {
+                         version = value;
+                    }
+               } catch (error) {
+                    console.error(error);
+               }
 
-          //Take the first 8
-          images = images.slice(0, 8);
-
-          //Duplicate
-          images = images.reduce(function(res, current, index, array) {
-               return res.concat([current, current]);
-          }, []);
-
-          //Randomize again
-          images.sort(function() {
-               return 0.5 - Math.random()
-          });
-
-          let Images = [];
-          for (var i = 0; i < images.length; i++) {
-               Images.push(<Card key={i} compare={this.press.bind(this)} clickable={true} index={i} onFlipEnd={this.compare.bind(this)} source={images[i]}/>)
+               try {
+                    let response = await fetch('http://104.236.141.69/art/version.json');
+                    let responseJson = await response.json();
+                    //console.warn(responseJson.version + " : " + parseInt(version))
+                    return !(responseJson.version == parseInt(version));
+               } catch (error) {
+                    console.error(error);
+               }
           }
 
-          return Images;
+          const download = async() => {
+               //console.warn("downloading")
+               let response = await fetch('http://104.236.141.69/art/download.json');
+               let responseJson = await response.json();
+               await AsyncStorage.setItem("version", JSON.stringify(responseJson.version));
+
+               //console.warn("downloaded: " + JSON.stringify(responseJson));
+               return responseJson.images;
+          }
+
+          const save = async(images) => {
+               //console.warn("saving: " + JSON.stringify(images))
+               await AsyncStorage.setItem("images", JSON.stringify(images));
+          }
+
+          const get = async() => {
+               let response = await AsyncStorage.getItem("images");
+               //console.warn("getting: " + response);
+               return JSON.parse(response);
+          }
+
+          const that = this;
+
+          const generate = async() => {
+               get().then(function(images) {
+                    //console.warn("Generating: " + JSON.stringify(images))
+                    //Randomize the entire array
+                    images.sort(function() {
+                         return 0.5 - Math.random()
+                    });
+
+                    //console.warn("Randomize: " + JSON.stringify(images));
+
+                    //Take the first 8
+                    images = images.slice(0, 8);
+
+                    //Duplicate
+                    images = images.reduce(function(res, current, index, array) {
+                         return res.concat([current, current]);
+                    }, []);
+
+                    //Randomize again
+                    images.sort(function() {
+                         return 0.5 - Math.random()
+                    });
+
+                    let Images = [];
+                    for (var i = 0; i < images.length; i++) {
+                         Images.push(<Card key={i} compare={that.press.bind(that)} clickable={true} index={images[i].id} onFlipEnd={that.compare.bind(that)} source={images[i].source}/>)
+                    }
+
+                    that.setState({images: Images, imagesData: images, loading: false});
+               })
+          }
+
+          update().then(function(doUpdate) {
+               if (doUpdate) {
+                    download().then(function(images) {
+                         save(images).then(function() {
+                              generate();
+                         });
+                    });
+               } else {
+                    generate();
+               }
+          });
      }
 
      reset() {
@@ -111,6 +203,52 @@ export default class GameBard extends Component {
           return (
                <ScrollView>
                     <View style={styles.container}>{this.state.images}</View>
+                    <Modal transparent={true} animationType="slide" visible={this.state.modalVisible} onRequestClose={() => {
+                         this.setModalVisible(false);
+                    }}>
+                         <TouchableOpacity style={styles.centerModal} activeOpacity={1} onPress={() => {
+                              this.setModalVisible(false);
+                         }}>
+                              <ScrollView directionalLockEnabled={true} contentContainerStyle={styles.centerModal}>
+                                   <TouchableWithoutFeedback>
+                                        <View style={{
+                                             justifyContent: 'center',
+                                             margin: 40
+                                        }}>
+                                             <View style={{
+                                                  elevation: 1
+                                             }}>
+                                                  <Image style={{
+                                                       height: 200,
+                                                       width: null,
+                                                       borderTopLeftRadius: 2,
+                                                       borderTopRightRadius: 2
+                                                  }} source={{
+                                                       uri: this.state.modalSource
+                                                  }}/>
+                                             </View>
+
+                                             <View style={{
+                                                  backgroundColor: "white",
+                                                  padding: 24,
+                                                  borderBottomLeftRadius: 2,
+                                                  borderBottomRightRadius: 2,
+                                                  elevation: 1
+                                             }}>
+                                                  <Text style={{
+                                                       fontSize: 24,
+                                                       marginBottom: 8,
+                                                       color: "black"
+                                                  }}>{this.state.modalTitle}</Text>
+                                                  <Text style={{
+                                                       fontSize: 15
+                                                  }}>{this.state.modalDescription}</Text>
+                                             </View>
+                                        </View>
+                                   </TouchableWithoutFeedback>
+                              </ScrollView>
+                         </TouchableOpacity>
+                    </Modal>
                </ScrollView>
           );
      }
@@ -128,6 +266,10 @@ const styles = StyleSheet.create({
           flex: 1,
           flexDirection: 'row',
           flexWrap: 'wrap'
+     },
+     centerModal: {
+          flex: 1,
+          justifyContent: 'center'
      }
 });
 
